1f4e069b17fc83985ce4b5547cb4bced87021644
Jon Carstens <jjcarstens@me.com>
2021-12-20 18:11:56 -0700

    Use SPDX license names
    [ci skip]

--- mix.exs
+++ mix.exs
@@ -91,7 +91,7 @@ defmodule NervesSystemOsd32mp1.MixProject do
   defp package do
     [
       files: package_files(),
-      licenses: ["Apache 2.0"],
+      licenses: ["Apache-2.0"],
       links: %{"GitHub" => @source_url}
     ]
   end
