e982904c98043b7fc80897fde7f3535b8a7a15f3
masahiro-999 <masahiro999@outlook.com>
2024-09-01 20:49:56 +0900

    change app name

--- mix.exs
+++ mix.exs
@@ -1,8 +1,8 @@
-defmodule NervesSystemOsd32mp1.MixProject do
+defmodule CustomStm32mp135d.MixProject do
   use Mix.Project
 
-  @github_organization "nerves-project"
-  @app :nerves_system_osd32mp1
+  @github_organization "masahiro-999"
+  @app :custom_stm32mp135d
   @source_url "https://github.com/#{@github_organization}/#{@app}"
   @version Path.join(__DIR__, "VERSION")
            |> File.read!()
